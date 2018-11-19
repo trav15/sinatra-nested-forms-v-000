ee2de7f57e97fa0b013a82239e6553c23590f28d
Travis T <travistokuyama@gmail.com>
2018-11-19 22:30:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,21 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/show' do
+      @student = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+
+      @ships = Ship.all
+
+      erb :'pirates/show'
+    end
 
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+  @@pirates = []
+
+  def initialize(params)
+    @name = params[:name]
+    @height = params[:height]
+    @weight = params[:weight]
+    @@pirates << self
+  end
+
+  def self.all
+    @@pirates
+  end
 end
